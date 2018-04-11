3e72c758444e6e09c5a46d0ca55c5ee19531f695
IAmWinyl <serra.abak@gmail.com>
2018-04-10 21:45:21 -0400

    Added comment documentation

--- FuzzerProject/Fuzzer.java
+++ FuzzerProject/Fuzzer.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 public class Fuzzer {
 
+    // Class variables
      private Random random = new SecureRandom();
      private Random rand = new Random();
      private int count = 1;
@@ -19,51 +20,79 @@ public class Fuzzer {
      private int imglength;
      private int excode;
      int b = 0;
+
+     // Description...
      public File mutate(File in, int start, int length) throws IOException  
      {
          //System.out.println("inp.length= "+ ((int) in.length())+" name= "+in.getName());
-         byte[] data = new byte[(int) in.length()];
-         DataInputStream din = new DataInputStream(new FileInputStream(in));
-         din.readFully(data);
-         
+
+        // Allocate byte array for image
+        byte[] data = new byte[(int) in.length()];
+        // Fill array with image data
+        DataInputStream din = new DataInputStream(new FileInputStream(in));
+        din.readFully(data);
+
+        // Alternate between....
+        // Randomizing image file
         if (b==0){
             fuzz(data, start, length);
             b=1;
-         }      
-        else{
+        }
+        // Growing image file   
+        else {
+            // length 
             int len = rand.nextInt(99990) + 99980;
             data=fuzz1(data, len);
             b=0;
         }
 
-         String name = "fuzz_" + count + "_" + in.getName();
-         File fout = new File(name);
-         FileOutputStream  out = new FileOutputStream(fout);
-         out.write(data);
-         out.close();
-         din.close();
-         count++;
-         return fout;
+        // Output image to file
+        String name = "fuzz_" + count + "_" + in.getName();
+        File fout = new File(name);
+        FileOutputStream  out = new FileOutputStream(fout);
+        out.write(data);
+
+        out.close();
+        din.close();
+
+        // Increase file name count
+        count++;
+
+        return fout;
      }
+
      // Modifies byte array in place
      public void fuzz(byte[] in, int start, int length) {
- 
-         byte[] fuzz = new byte[length];
-         random.nextBytes(fuzz);
-         System.arraycopy(fuzz, 0, in, start, fuzz.length);
+        // Allocate image array
+        byte[] fuzz = new byte[length];
+
+        // Randomly fill with bytes
+        random.nextBytes(fuzz);
+
+        // Copy randomized array to main image array
+        System.arraycopy(fuzz, 0, in, start, fuzz.length);
      }
+
      //Modifies by adding rand number of bytes
      public byte[] fuzz1(byte[] in, int length){
+        // Allocate space to grow
         byte[] fuzz = new byte[length];
         byte[] dest = new byte[in.length + fuzz.length + fuzz.length + fuzz.length];
+
+        // Fill with random bytes
         random.nextBytes(fuzz);
+
         int l = in.length - 1 + fuzz.length;
+
+        // Copy original array to destination
         System.arraycopy(in, 0, dest, 0, in.length);
+        // Concatenate destination and fuzz
         System.arraycopy(fuzz, 0, dest, in.length-1, fuzz.length);
 
         return dest;
      }
-     //run target program with mutated image
+
+     // Run target program with mutated image
      public void executeTarget(File testFile){
         char[] buffer = new char[1024];
        //run jpg2pdf.cpp with image.jpg
@@ -96,37 +125,43 @@ public class Fuzzer {
                 System.out.println("Did not crash- deleting: " + testFile.getName());
                 testFile.delete();
             }
-        }catch(Exception ex) {
+        } catch(Exception ex) {
             ex.printStackTrace();
         }
      }
      
      public static void main(String[] args) {
+        // Local variables
+       int ret, i = 0;
+
        Fuzzer hw = new Fuzzer();
        
+       // Read user input for image file name
        System.out.println("Enter img file name: ");
        Scanner scan = new Scanner(System.in);
        String filename = scan.next();
        
-       File inp= new File(filename);
+       // Open image
+       File inp = new File(filename);
+
+       // Get image length
        hw.imglength = (int) inp.length();
-       System.out.println("The file name: "+ inp.getName()+" "+hw.imglength);
-       
-       int ret,i=0;
-       try{
-         while (i<3){
+       System.out.println("The file name: " + inp.getName() + " " + hw.imglength);
+
+       try {
+         while (i<3) {
             i++;
             int start = hw.rand.nextInt(50) + 1; //max=10, min=1
             int len = hw.rand.nextInt(1050) + 1;
             int range = start + len;
-            if(range> hw.imglength) continue;
+            if(range > hw.imglength) continue;
 
             hw.muteFile = hw.mutate(inp, start, len);
             //System.out.println(i+ " Shown: "+ hw.muteFile + "start & len:"+start+" & "+len);
             hw.executeTarget(hw.muteFile);
             //if(hw.excode==139) break;
           }
-       }catch(IOException e){
+       } catch(IOException e) {
          e.printStackTrace();
          System.out.println("Nothing..:] not mutated");
        }
